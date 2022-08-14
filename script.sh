7b1fe88d8bb50024d8b81b3db54037ef3f2da775
Ashish Kumar <ashishkr23438@gmail.com>
2022-08-14 19:54:49 +0000

    rename master -> main in script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 postgresVer=("11" "12" "13" "14")
 postgisVer=("3.2")
-pgroutingVer=("3.0" "3.1" "3.2" "3.3" "master" "develop")
+pgroutingVer=("3.0" "3.1" "3.2" "3.3" "main" "develop")
 
-include=("10-2.5-master" "10-2.5-develop" "11-2.5-master" "11-2.5-develop")
-exclude=("11-3.2-master" "11-3.2-develop")
+include=("10-2.5-main" "10-2.5-develop" "11-2.5-main" "11-2.5-develop")
+exclude=("11-3.2-main" "11-3.2-develop")
 
 for postgres in ${postgresVer[@]}; do
     for postgis in ${postgisVer[@]}; do
